6ff2a26393cbac28145c24c566cd0353eed5022f
umbrella-leaf <2313678365@qq.com>
2024-07-26 02:19:35 +0800

    add corresponding for web-search function

--- test/chat_test.py
+++ test/chat_test.py
@@ -5,11 +5,11 @@ from DouBaoChat.bot import ChatBot
 async def main():
     chatbot = ChatBot(
         api_key="70f78b22-8f4e-42ef-a98f-0d6b1b6b2cd3",
-        endpoint="doubao_pro_32k"
+        endpoint="doubao_pro_32k",
+        bot_id="bot-20240726015052-ft6f2"
     )
-    print(await chatbot.ask(message="请你告诉我1+1等于几？"))
+    print(await chatbot.ask(message="目前拜登有什么新闻？"))
     print(await chatbot.ask(message="我刚才问你什么？"))
-    print(await chatbot.ask(message="我刚才问你的数学题的答案是多少？"))
 
 
 loop = asyncio.new_event_loop()
